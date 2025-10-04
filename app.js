71dbcd9aefac905dd303e98fea39c7425970c1be
armandoneto91 <kikobatery@hotmail.com>
2025-10-03 22:54:55 -0300

    commit assinado

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-
 const server = http.createServer((req, res) => {
   if (req.url === "/" && req.method === "GET") {
     res.writeHead(200, { "Content-Type": "text/plain" });
